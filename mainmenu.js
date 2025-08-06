a5ed15b5498385c5549500c7d9539b9acca520df
TrevorDaBest <135571783+TrevorDaBest@users.noreply.github.com>
2025-08-05 17:50:30 -0700

    Update mainmenu.js

--- mainmenu.js
+++ mainmenu.js
@@ -1,5 +1,6 @@
-var userInfo = JSON.parse(sessionStorage.getItem("userInfo"))
+var userInfo = JSON.parse(localStorage.getItem("userInfo"))
 var welcomeText = document.getElementById("welcome")
 
 welcomeText.textContent = "Welcome, " + userInfo.username + "!"
+
 
